3ecfa130cffd6f0b73b13df1928bcde777c30c9a
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2025-02-23 22:38:58 +0100

    Add project files.

---
+++ CompanyManager.RestConApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyManager.RestConApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
